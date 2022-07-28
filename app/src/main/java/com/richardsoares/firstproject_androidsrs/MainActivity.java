124b934dd095946ee105ca14f0c8a5effac10f8a
Richard Soares <soaresr870@gmail.com>
2022-07-28 20:10:03 -0300

    Testing activy view component and TextView

--- app/src/main/java/com/richardsoares/firstproject_androidsrs/MainActivity.java
+++ app/src/main/java/com/richardsoares/firstproject_androidsrs/MainActivity.java
@@ -1,14 +1,29 @@
 package com.richardsoares.firstproject_androidsrs;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.annotation.Nullable;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends Activity {
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        List<String> alunos = new ArrayList<>(
+                Arrays.asList("Richard", "Franciele", "José"));
+
+        TextView primeiroAluno = findViewById(R.id.textView);
+        TextView segundoAluno = findViewById(R.id.textView2);
+        TextView terceiroAluno = findViewById(R.id.textView3);
+        primeiroAluno.setText(alunos.get(0));
+        segundoAluno.setText(alunos.get(1));
+        terceiroAluno.setText(alunos.get(2));
     }
 }
