1cca66a487e2bdecfadde19aeab5dd0a48ee07dc
Richard Soares <soaresr870@gmail.com>
2022-07-29 23:54:06 -0300

    Inserted icon in fab and alter activy_main and added AppCompat

--- app/src/main/java/com/richardsoares/firstproject_androidsrs/MainActivity.java
+++ app/src/main/java/com/richardsoares/firstproject_androidsrs/MainActivity.java
@@ -1,21 +1,22 @@
 package com.richardsoares.firstproject_androidsrs;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("Lista de Alunos");
         List<String> alunos = new ArrayList<>(
                 Arrays.asList("Richard", "Franciele", "José", "Maria", "Anna", "Carlos"));
 
