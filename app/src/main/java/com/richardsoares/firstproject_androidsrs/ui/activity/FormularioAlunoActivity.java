30c43b15243c1941f0cb0156b82147ca72157d05
Richard Soares <soaresr870@gmail.com>
2022-07-30 00:27:59 -0300

    added new activity to save new student and refactoring code

---
+++ app/src/main/java/com/richardsoares/firstproject_androidsrs/ui/activity/FormularioAlunoActivity.java
@@ -0,0 +1,16 @@
+package com.richardsoares.firstproject_androidsrs.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.richardsoares.firstproject_androidsrs.R;
+
+public class FormularioAlunoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario_aluno);
+    }
+}
