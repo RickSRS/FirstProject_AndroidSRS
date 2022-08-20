a572088bfb66c98e56558d668ba856c140e61a0e
Richard Soares <soaresr870@gmail.com>
2022-08-19 23:38:45 -0300

    implemented query to get phone one

--- app/src/main/java/com/richardsoares/firstproject_androidsrs/database/dao/TelefoneDAO.java
+++ app/src/main/java/com/richardsoares/firstproject_androidsrs/database/dao/TelefoneDAO.java
@@ -1,10 +1,14 @@
 package com.richardsoares.firstproject_androidsrs.database.dao;
 
 import androidx.room.Dao;
+import androidx.room.Query;
 
 import com.richardsoares.firstproject_androidsrs.model.Telefone;
 
 @Dao
 public interface TelefoneDAO {
-    Telefone buscaPrimeiroTelefoneDoAluno();
+    @Query("SELECT t.* FROM Telefone t " +
+            "JOIN Aluno a ON t.alunoId = a.id " +
+            "WHERE t.alunoId = :alunoId LIMIT 1")
+    Telefone buscaPrimeiroTelefoneDoAluno(int alunoId);
 }
